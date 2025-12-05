e70ef7ffbc115362c8b3039e663c755f41f6d98a
Vahid Hoseini <vahid09390292608@gmail.com>
2025-12-05 16:04:01 +0330

    Add nullable imports for fragments

--- android/src/com/zargidi/ccar/android/GameOverFragment.java
+++ android/src/com/zargidi/ccar/android/GameOverFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- android/src/com/zargidi/ccar/android/MainMenuFragment.java
+++ android/src/com/zargidi/ccar/android/MainMenuFragment.java
@@ -3,6 +3,7 @@ package com.zargidi.ccar.android;
 import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
